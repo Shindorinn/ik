38403009d7e22823600b9d444ed61f08d5bda390
Shindorinn <du.a.meijer@googlemail.com>
2017-10-10 15:54:56 +0200

    WIP

--- ik.m
+++ ik.m
@@ -1,81 +1,101 @@
-pkg load symbolic
+1;
+
+function T = getTMatrix(angle_prox, angle_inter, angle_distal, len_prox, len_inter, len_distal, lb_angle_prox, ub_angle_prox, lb_angle_inter, ub_angle_inter, lb_angle_distal, ub_angle_distal)
+  T = zeros(4)
+  if (angle_prox < lb_angle_prox)
+    angle_prox = lb_angle_prox
+  elseif (angle_prox > ub_angle_prox)
+    angle_prox = ub_angle_prox
+  endif
+    
+  if (angle_inter < lb_angle_inter)
+    angle_prox = lb_angle_inter
+  elseif (angle_inter > ub_angle_inter)
+    angle_inter = ub_angle_inter
+  endif
+    
+  if (angle_distal < lb_angle_distal)
+    angle_distal = lb_angle_distal
+  elseif (angle_distal > ub_angle_distal)
+    angle_distal = ub_angle_distal
+  endif
+  
+  T_0_1 = zeros(4)
+  T_1_2 = zeros(4)
+  T_2_3 = zeros(4)
+
+  T_0_1(0) = cos(angle_prox)
+  T_0_1(1) = -sin(angle_prox)
+  T_0_1(3) = len_prox*cos(angle_prox)
+  T_0_1(4) = sin(angle_prox)
+  T_0_1(5) = cos(angle_prox)
+  T_0_1(7) = len_prox*sin(angle_prox) 
+
+  T_1_2(0) = cos(angle_inter)
+  T_1_2(1) = -sin(angle_inter)
+  T_1_2(3) = len_prox*cos(angle_inter)
+  T_1_2(4) = sin(angle_inter)
+  T_1_2(5) = cos(angle_inter)
+  T_1_2(7) = len_prox*sin(angle_inter)
+
+  T_2_3(0) = cos(angle_distal)
+  T_2_3(1) = -sin(angle_distal)
+  T_2_3(3) = len_prox*cos(angle_distal)
+  T_2_3(4) = sin(angle_distal)
+  T_2_3(5) = cos(angle_distal)
+  T_2_3(7) = len_prox*sin(angle_distal)
+
+  T = T_0_1 * T_1_2 * T_2_3
+
+endfunction
+
+function inv_J_q = calcInvJacobian(target, angle_prox, angle_inter, angle_distal)
+
+  %target = { x, y }
+  q = { angle_prox, angle_inter }
+
+  J_q = zeros(2)
+
+  J_q(0) = - len_prox * sin(angle_prox) - len_inter * sin(angle_prox * angle_inter ) - len_distal * sin( angle_prox + (5/3) * angle_inter )
+  J_q(1) = - len_inter * sin(angle_prox * angle_inter ) - len_distal * sin( angle_prox + (5/3) * angle_inter ) 
+  J_q(2) = len_prox * cos(angle_prox) + len_inter * cos(angle_prox * angle_inter ) - len_distal * cos( angle_prox + (5/3) * angle_inter )
+  J_q(3) = len_inter * cos(angle_prox * angle_inter ) - len_distal * cos( angle_prox + (5/3) * angle_inter )
+
+  D = J_q(0)*J_q(3) - J_q(1)*J_q(2)
+
+  inv_J_q = 1/D * J_q
+
+endfunction
+
 
 % Visualization
 %img = figure(1)
 %clf
 %hold on
 
-% Object O = {(x,y,z) in R | y <= -2}
-
-len_prox = 44.6
-len_inter = 26.3
-len_distal = 17.4
-
-syms angle_prox
-syms angle_inter
-syms angle_distal
+global len_prox = 44.6
+global len_inter = 26.3
+global len_distal = 17.4
 
 % Restrictions TODO : Triple-check the restrictions
 % -pi / 3 <= Theta_M <= pi /3
 % -2 pi / 3 <= Theta_P <= 0
 % -2 pi / 3 <= Theta _D <= 0
-lb_angle_prox = -pi /3
-ub_angle_prox = pi/3
 
-assume(lb_angle_prox <= angle_prox)
-assumeAlso(angle_prox <= ub_angle_prox) 
-assumptions
+global lb_angle_prox   = -pi /3
+global ub_angle_prox   = pi/3
+global lb_angle_inter  = -2*pi/3
+global ub_angle_inter  = 0
+global lb_angle_distal = -2*pi/3
+global ub_angle_distal = 0
 
-%
-%      R  t
-% T = 
-%      0  1
-%
+target = {len_prox + len_inter + len_distal, -2}
 
-T_0_1 = zeros(4)
-T_1_2 = zeros(4)
-T_2_3 = zeros(4)
+global error_margin = 0.001
 
-T_0_1(0) = cos(angle_prox)
-T_0_1(1) = -sin(angle_prox)
-T_0_1(3) = len_prox*cos(angle_prox)
-T_0_1(4) = sin(angle_prox)
-T_0_1(5) = cos(angle_prox)
-T_0_1(7) = len_prox*sin(angle_prox) 
+getTMatrix(1,1,1,2,2,2,0,0,0,0,0,0)
 
-T_1_2(0) = cos(angle_inter)
-T_1_2(1) = -sin(angle_inter)
-T_1_2(3) = len_prox*cos(angle_inter)
-T_1_2(4) = sin(angle_inter)
-T_1_2(5) = cos(angle_inter)
-T_1_2(7) = len_prox*sin(angle_inter)
-
-T_2_3(0) = cos(angle_distal)
-T_2_3(1) = -sin(angle_distal)
-T_2_3(3) = len_prox*cos(angle_distal)
-T_2_3(4) = sin(angle_distal)
-T_2_3(5) = cos(angle_distal)
-T_2_3(7) = len_prox*sin(angle_distal)
-
-T = T_0_1 * T_1_2 * T_2_3
-
-sym x
-sym y
-
-target = { x, y }
-
-q = { angle_prox, angle_inter }
-
-J_q = zeros(2)
-
-J_q(0) = - len_prox * sin(angle_prox) - len_inter * sin(angle_prox * angle_inter ) - len_distal * sin( angle_prox + (5/3) * angle_inter )
-J_q(1) = - len_inter * sin(angle_prox * angle_inter ) - len_distal * sin( angle_prox + (5/3) * angle_inter ) 
-J_q(2) = len_prox * cos(angle_prox) + len_inter * cos(angle_prox * angle_inter ) - len_distal * cos( angle_prox + (5/3) * angle_inter )
-J_q(3) = len_inter * cos(angle_prox * angle_inter ) - len_distal * cos( angle_prox + (5/3) * angle_inter )
-
-D = J_q(0)*J_q(3) - J_q(1)*J_q(2)
-
-inv_J_q = 1/D * J_q
+% Object O = {(x,y,z) in R | y <= -2}
 
 % IK Method : 
 % nextAnglesGuess = currentAnglesGuess + inverse( J_q(i) )*( target - currentPosition ) 
