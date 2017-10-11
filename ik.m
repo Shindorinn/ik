f8a7b42b9bbadd2e0e887c1cacde88759aea98ab
Shindorinn <du.a.meijer@googlemail.com>
2017-10-11 16:28:35 +0200

    Changed matrix indices, TODO : Double-check if they are correct

--- ik.m
+++ ik.m
@@ -24,26 +24,26 @@ function T = getTMatrix(angle_prox, angle_inter, angle_distal, len_prox, len_int
   T_1_2 = zeros(4)
   T_2_3 = zeros(4)
 
-  T_0_1(0) = cos(angle_prox)
-  T_0_1(1) = -sin(angle_prox)
-  T_0_1(3) = len_prox*cos(angle_prox)
-  T_0_1(4) = sin(angle_prox)
-  T_0_1(5) = cos(angle_prox)
-  T_0_1(7) = len_prox*sin(angle_prox) 
+  T_0_1(1) = cos(angle_prox)
+  T_0_1(2) = -sin(angle_prox)
+  T_0_1(4) = len_prox*cos(angle_prox)
+  T_0_1(5) = sin(angle_prox)
+  T_0_1(6) = cos(angle_prox)
+  T_0_1(8) = len_prox*sin(angle_prox) 
 
-  T_1_2(0) = cos(angle_inter)
-  T_1_2(1) = -sin(angle_inter)
-  T_1_2(3) = len_prox*cos(angle_inter)
-  T_1_2(4) = sin(angle_inter)
-  T_1_2(5) = cos(angle_inter)
-  T_1_2(7) = len_prox*sin(angle_inter)
+  T_1_2(1) = cos(angle_inter)
+  T_1_2(2) = -sin(angle_inter)
+  T_1_2(4) = len_prox*cos(angle_inter)
+  T_1_2(5) = sin(angle_inter)
+  T_1_2(6) = cos(angle_inter)
+  T_1_2(8) = len_prox*sin(angle_inter)
 
-  T_2_3(0) = cos(angle_distal)
-  T_2_3(1) = -sin(angle_distal)
-  T_2_3(3) = len_prox*cos(angle_distal)
-  T_2_3(4) = sin(angle_distal)
-  T_2_3(5) = cos(angle_distal)
-  T_2_3(7) = len_prox*sin(angle_distal)
+  T_2_3(1) = cos(angle_distal)
+  T_2_3(2) = -sin(angle_distal)
+  T_2_3(4) = len_prox*cos(angle_distal)
+  T_2_3(5) = sin(angle_distal)
+  T_2_3(6) = cos(angle_distal)
+  T_2_3(8) = len_prox*sin(angle_distal)
 
   T = T_0_1 * T_1_2 * T_2_3
 
@@ -56,12 +56,12 @@ function inv_J_q = calcInvJacobian(target, angle_prox, angle_inter, angle_distal
 
   J_q = zeros(2)
 
-  J_q(0) = - len_prox * sin(angle_prox) - len_inter * sin(angle_prox * angle_inter ) - len_distal * sin( angle_prox + (5/3) * angle_inter )
-  J_q(1) = - len_inter * sin(angle_prox * angle_inter ) - len_distal * sin( angle_prox + (5/3) * angle_inter ) 
-  J_q(2) = len_prox * cos(angle_prox) + len_inter * cos(angle_prox * angle_inter ) - len_distal * cos( angle_prox + (5/3) * angle_inter )
-  J_q(3) = len_inter * cos(angle_prox * angle_inter ) - len_distal * cos( angle_prox + (5/3) * angle_inter )
+  J_q(1) = - len_prox * sin(angle_prox) - len_inter * sin(angle_prox * angle_inter ) - len_distal * sin( angle_prox + (5/3) * angle_inter )
+  J_q(2) = - len_inter * sin(angle_prox * angle_inter ) - len_distal * sin( angle_prox + (5/3) * angle_inter ) 
+  J_q(3) = len_prox * cos(angle_prox) + len_inter * cos(angle_prox * angle_inter ) - len_distal * cos( angle_prox + (5/3) * angle_inter )
+  J_q(4) = len_inter * cos(angle_prox * angle_inter ) - len_distal * cos( angle_prox + (5/3) * angle_inter )
 
-  D = J_q(0)*J_q(3) - J_q(1)*J_q(2)
+  D = J_q(1)*J_q(4) - J_q(2)*J_q(3)
 
   inv_J_q = 1/D * J_q
 
